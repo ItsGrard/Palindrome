b58fdc554d4029bb611b957701e0e5a6d61d86eb
ItsGrard <33222999+ItsGrard@users.noreply.github.com>
2017-11-28 12:25:20 +0100

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,3 +1,6 @@
+//Gerardo Marin
+//My code - NOT working
+
 #include <iostream>
 using namespace std;
 
@@ -10,23 +13,25 @@ int main(){
     cout << "Insert number: " << endl;
     	cin >> num;
     	cout << "num1 " << num<< endl;
-  n = num;
-    	cout << "num "<< num <<endl;
-    	cout << " n is  " << n << endl << endl;
+  n = num;  //Cloning num so I can modify "n"
+    	cout << "num "<< num <<endl;  //Just checking what compiler is doing and if its reading everything correctly
+    	cout << " n is  " << n << endl << endl; //Checking
     	do {
 
-    		digit = num%10;
-    	//	cout << num<< endl;
-    		rev = (rev*10)+digit;
-    		n = n/10;
-			cout << "digit         " << digit <<"      rev  " << rev <<"    n:  " << n << "      num     " << num << endl;    	
-    	}	while (n !=0);
+    		digit = num%10; //Taking last number to create a new one but reversed, problem is, it always takes the last
+    		
+    		rev = (rev*10)+digit; //Reversing the number
+    		n = n/10;		// This is made to exit the loop after number ends
+	
+		cout << "digit   " << digit <<"  rev  " << rev <<" n:  " << n << " num" << num << endl;    //Checking as well	
+
+	}	while (n !=0);
     	
     	
     	if (num == rev)
-    		 cout << "is palindrome" << endl;
+		cout << "is palindrome" << endl;
     	else 
-    		cout << "is not" << endl << endl;
+    		cout << "is not" << endl;
 
     	return 0;
     }
